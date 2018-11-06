680032b819163f10b339ad7f53acce5ed42d6acf
SBriscoe2 <shabris@tiscali.co.uk>
2018-11-06 16:24:35 +0000

    complete challenge #5: add a top up to the balance

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 class Oystercard
   attr_reader :balance
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(top_up_value)
+    @balance += top_up_value
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,11 +1,20 @@
+# frozen_string_literal: true
+
 require 'oyster_card'
 
 describe Oystercard do
-  it { is_expected.to respond_to :balance }
-end
+let(:add_top_up_money) { subject.top_up(20) }
 
-describe Oystercard do
+  it { is_expected.to respond_to :balance }
+
   it 'is expected to show a balance of 0' do
     expect(subject.balance).to eq 0
+  end
+
+  it { is_expected.to respond_to(:top_up).with(1) }
+
+  it 'is expected to show a balance of 40' do
+    2.times { add_top_up_money }
+    expect(subject.balance).to eq 40
   end
 end
