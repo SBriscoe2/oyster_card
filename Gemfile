007ddae3b837c6eef804f1b0a8e774a83398ec43
SBriscoe2 <shabris@tiscali.co.uk>
2018-11-06 15:52:24 +0000

    Complete challenge #4: Added balance

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 ruby '2.5.1'
 
 group :development, :test do
-  gem "rspec"
+  gem 'rspec'
 end
 
 # gem "rails"
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,11 @@
 require 'oyster_card'
 
 describe Oystercard do
+  it { is_expected.to respond_to :balance }
+end
 
+describe Oystercard do
+  it 'is expected to show a balance of 0' do
+    expect(subject.balance).to eq 0
+  end
 end
